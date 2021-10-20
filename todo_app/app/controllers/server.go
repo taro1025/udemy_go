f6143d7a6e8c4ff5ab843a8053cba308834e6384
taro1025 <game4967@gmail.com>
2021-10-20 12:26:39 +0900

    create layout

--- todo_app/app/controllers/server.go
+++ todo_app/app/controllers/server.go
@@ -1,11 +1,24 @@
 package controllers
 
 import (
+	"fmt"
+	"html/template"
 	"net/http"
 	"practiceGo/todo_app/config"
 )
 
+func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	//template.Must...Create cache of templates
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(writer, "layout", data)
+}
+
 func StartMainServer() error {
 	http.HandleFunc("/", top)
-	return http.ListenAndServe(":" + config.Config.Port, nil)
+	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
